08bae5768b7ecfe6f79defadc7d5e6aa5d124f9a
Kyle Owsen <me@kylebyte.com>
2021-03-29 01:22:11 -0700

    Clarified docs

--- Root.gd
+++ Root.gd
@@ -5,7 +5,15 @@ extends Node2D
 var toggle := Rx.new(false)
 var counter := Rx.new(0)
 
-# You can map values using a funcref. In this case we have to
+# This maps the counter to a Vector2 to use as the size of our button.
+var button_size = counter.pipe([
+	# This is the closest thing GDScript's gonna get to a lambda function!
+	# They work well enough for pure mapping or filtering functions.
+	RxMap.new("counter => counter * 40 + 120"),
+	RxMap.new("width => Vector2(width, 50)")
+])
+
+# You can also map values using a funcref. In this case we have to
 # because ternary operators don't work in my janky lambdas.
 var title = toggle.pipe([
 	RxMap.new(funcref(self, "_get_title"))
@@ -16,17 +24,10 @@ var button_text = RxCombine.new([title, counter]).pipe([
 	# This only lets the value emit once every frame. This is helpful
 	# if something were to update title and counter at the same time,
 	# but we didn't want to consider the state where one's been updated
-	# but the other hasn't.
+	# but the other hasn't. (imagine moving to a new room in a roguelike
+	# and deprecating the amount of food)
 	RxDebounce.new(),
-	# This is the closest thing GDScript's gonna get to a lambda function!
-	# They work well enough for simple, pure mapping or filtering functions.
 	RxMap.new("x => x[0] + ': ' + String(x[1])")
-])
-
-# This maps the counter to a Vector2 to use as the size of our button.
-var button_size = counter.pipe([
-	RxMap.new("counter => counter * 40 + 120"),
-	RxMap.new("width => Vector2(width, 50)")
 ])
 
 # This is to show that even though we update title and counter on every click,
